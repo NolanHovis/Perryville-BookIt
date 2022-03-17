ea1a416cab0723a51f1a3ccdbd1c4a8df04126fa
Nolan Hovis <nolan.hovispersonal@gmail.com>
2022-03-17 12:59:59 -0500

    ROutes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "BookIt_PerryVille_production"
+  # config.active_job.queue_name_prefix = "book_it_perryville_production"
 
   config.action_mailer.perform_caching = false
 
